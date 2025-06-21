c1902d49c08e78f95dafcb9768b5275aa40cc177
carolis <redrumjedi@gmail.com>
2025-06-21 13:28:54 -0300

    feat: add base REPL

--- app/main.rb
+++ app/main.rb
@@ -1,8 +1,23 @@
-$stdout.write("$ ")
+class MyShell
+  attr_reader :invalid_command
+  attr_accessor :command, :args, :end
+  def initialize
+    @command, *args = nil
+    @invalid_command = true
+    @end = false
+  end
+end
 
-invalid_command = true
-command, *args = gets.chomp.split(" ")
+shell = MyShell.new
 
-unless invalid_command == false
-  puts "#{command}: command not found"
+until shell.end
+  $stdout.write("$ ")
+  unless shell.invalid_command == false
+    shell.command = gets.chomp.split(" ")
+    if shell.command != []
+      puts "#{shell.command[0]}: command not found"
+    else
+      shell.end = true
+    end
+  end
 end
